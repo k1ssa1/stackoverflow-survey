9ec2887ac152c5e7768df33a27b7a278d2cb9a10
k1ssa1 <samikissai.edu@gmail.com>
2023-01-21 22:27:36 +0100

    exploring series access

--- dataframes.py
+++ dataframes.py
@@ -7,6 +7,25 @@ people = {
     "job_title": ["CEO","CTO","CFO"]
 }
 
+# displaying the entire dataframes
 df = pd.DataFrame(people)
 
+# accesing series in dataframes in two ways
+serie1 = df['lName']
+serie2 = df.job_title
+
+# accesing multiple series
+multi_series = df[["lName","fName"]]
+
+# printing the display
+print("******* displaying the dataframe *******")
 print(df)
+print("\n")
+print("******* displaying the first serie *******")
+print(serie1)
+print("\n")
+print("******* displaying the second serie *******")
+print(serie2)
+print("\n")
+print("******* displaying multi serie *******")
+print(multi_series)
